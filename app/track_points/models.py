1e6f90c83552de116d320f599013a9c34f1a456b
Raphael Lehmann <raphael@rleh.de>
2016-02-21 23:07:16 +0100

    add track_points db model

---
+++ app/track_points/models.py
@@ -0,0 +1,49 @@
+from app import db
+
+import geoalchemy2
+from shapely import wkb
+
+
+class TrackPoints(db.Model):
+    __tablename__ = 'track_points'
+    id = db.Column('id', db.BigInteger, db.ForeignKey('tracks.id'), nullable=False, index=True, primary_key=True)
+    geom = db.Column(geoalchemy2.Geometry('POINT'), nullable=False, index=True)  # geo index?
+    time = db.Column(db.TIMESTAMP, nullable=False)
+    altitude = db.Column(db.Numeric(16, 8))
+    accuracy = db.Column(db.Numeric(11, 8))
+    velocity = db.Column(db.Numeric(11, 8))
+    vibrations = db.Column(db.Numeric(16, 8))
+
+    def __init__(self, id, lat, lon, time, altitude, accuracy, velocity, vibrations):
+        self.id = id
+        self.geom = 'POINT(' + lon + ' ' + lat + ')'
+        self.time = time
+        self.altitude = altitude
+        self.accuracy = accuracy
+        self.velocity = velocity
+        self.vibrations = vibrations
+
+    def __repr__(self):
+        return '<trackpoint %i: %s>' % self.id, self.geom
+
+    def to_dict_short(self):
+        point = wkb.loads(bytes(self.geom.data))
+        return {
+            'name': self.name,
+            'lat': point.y,
+            'lon': point.x,
+            'time': self.time,
+               }
+
+    def to_dict_long(self):
+        point = wkb.loads(bytes(self.geom.data))
+        return {
+            'name': self.name,
+            'lat': point.y,
+            'lon': point.x,
+            'time': self.time,
+            'alt': self.altitude,
+            'accuracy': self.accuracy,
+            'velocity': self.velocity,
+            'vibrations': self.vibrations,
+               }
