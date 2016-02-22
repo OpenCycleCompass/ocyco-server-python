ab2adc94470d8a786a48ee2b5fe73ab14a40e104
Raphael Lehmann <raphael@rleh.de>
2016-02-22 05:09:11 +0100

    error handlers show custom message now

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,37 @@ db = SQLAlchemy(app)
 from app.tracks.views import mod as track_mod
 
 
+# TODO: central error handlers
+@app.errorhandler(400)
+def not_found(error):
+    return jsonify({
+        'status': 'Bad Request',
+        'error': error.description
+    }), 400
+
+
+@app.errorhandler(401)
+def not_found(error):
+    return jsonify({
+        'status': 'Unauthorized',
+        'error': error.description
+    }), 401
+
+
 @app.errorhandler(404)
 def not_found(error):
-    # TODO: central error handlers
-    return jsonify(error="Not found"), 404
+    return jsonify({
+        'status': 'Not found',
+        'error': error.description
+    }), 404
+
+
+@app.errorhandler(500)
+def not_found(error):
+    return jsonify({
+        'status': 'Internal error',
+        'error': error.description
+    }), 500
 
 
 app.register_blueprint(track_mod)
