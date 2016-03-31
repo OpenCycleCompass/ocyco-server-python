c354242c78600b4167d1f70a10598d75d12f52e2
Raphael Lehmann <raphael@rleh.de>
2016-03-31 02:39:47 +0200

    database models: add unique constraints

--- app/cost_static/models.py
+++ app/cost_static/models.py
@@ -3,17 +3,15 @@ from app import db
 
 class CostStatic(db.Model):
     __tablename__ = 'cost_static'
-    _local_id = db.Column('local_id', db.BigInteger, db.Sequence('track_points_local_id_seq'), primary_key=True,
+    _local_id = db.Column('local_id', db.BigInteger, db.Sequence('cost_static_local_id_seq'), primary_key=True,
                           unique=True, autoincrement=True)
-    id = db.Column('id', db.BigInteger, db.ForeignKey('way_types.id'), nullable=False, index=True)
-    profile = db.Column('profile', db.BigInteger, db.ForeignKey('profiles.id'), nullable=False, index=True)
+    id = db.Column('id', db.BigInteger, db.ForeignKey('way_types.id'), nullable=False)
+    profile = db.Column('profile', db.BigInteger, db.ForeignKey('profiles.id'), nullable=False)
     cost_forward = db.Column(db.Numeric(16, 8))
     cost_reverse = db.Column(db.Numeric(16, 8))
 
     # unique constraint: (id, profile)-tupel
-    # TODO
-    # db.UniqueConstraint('cost_static.id', 'cost_static.profile')
-    # TODO index? -> !
+    __table_args__ = (db.Index('cost_static_uniq_idx', id, profile, unique=True), )
 
     def __init__(self, id, profile, cost_forward, cost_reverse):
         self.id = id  # way_type id
--- app/profile_descriptions/models.py
+++ app/profile_descriptions/models.py
@@ -3,14 +3,14 @@ from app import db
 
 class ProfileDescriptions(db.Model):
     __tablename__ = 'profile_descriptions'
-    _local_id = db.Column('local_id', db.BigInteger, db.Sequence('track_points_local_id_seq'), primary_key=True,
+    _local_id = db.Column('local_id', db.BigInteger, db.Sequence('profile_descriptions_local_id_seq'), primary_key=True,
                           unique=True, autoincrement=True)
-    id = db.Column('id', db.BigInteger, db.ForeignKey('profiles.id'), nullable=False, index=True)
+    id = db.Column('id', db.BigInteger, db.ForeignKey('profiles.id'), nullable=False)
     language = db.Column(db.Text, nullable=False)
     description = db.Column(db.Text, nullable=False)
 
     # Unique constraint: (id, language must be unique)
-    # TODO
+    __table_args__ = (db.Index('profile_desc_uniq_idx', id, language, unique=True), )
 
     default_language = 'en-US'
 
