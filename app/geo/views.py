ec79ec0a458480195a0c8d1a330bddd7f0d29632
Raphael Lehmann <raphael@rleh.de>
2016-04-17 23:38:32 +0200

    geo resource: fix some mistakes, add documentation; [wip]

--- app/geo/views.py
+++ app/geo/views.py
@@ -9,15 +9,22 @@ mod = Blueprint('geo', __name__)
 
 
 def coordinates_from_linestring(linestring):
+    """
+    parse WKT linestring and return list of dicts of lat and lon values for jsonify()
+    :param linestring: WKT linestring
+    :return: a list of dict containing lat and lon values
+    """
     # remove LINESTRING and brackets
     linestring = linestring[11:-1]
+    # split in (lat lon) pairs
     linestring = linestring.split(',')
     coordinates = []
     for coordinate in linestring:
+        # split lat and lon value
         coordinate = coordinate.split(' ')
         coordinates.append({
-            'lat': coordinate[0],
-            'lon': coordinate[1]
+            'lon': coordinate[0],
+            'lat': coordinate[1]
         })
     return coordinates
 
@@ -39,19 +46,11 @@ def get_geo():
     lat_max = max(json.get('start_lat'), json.get('end_lat'))
     lon_min = min(json.get('start_lon'), json.get('end_lon'))
     lon_max = max(json.get('start_lon'), json.get('end_lon'))
-
-    query2 = text('SELECT ways.gid AS id, ST_AsText(ways.the_geom) AS geom, classes.cost AS cost '
-                 'FROM ways JOIN classes ON ways.class_id = classes.id '
-                 'WHERE ways.the_geom && ST_MakeEnvelope(:lon_min, :lat_min, :lon_max, :lat_max, 4326) '
-                 'AND classes.profile = :profile '
-                 'LIMIT 10000;')
-
     query = text('SELECT gid, osm_id, ST_AsText(ways.the_geom) '
                  'FROM ways '
                  'WHERE the_geom && ST_MakeEnvelope(:lon_min, :lat_min, :lon_max, :lat_max, 4326) '
                  'LIMIT 100;')
     query_result = db.engine.execute(query, lon_min=lon_min, lat_min=lat_min, lon_max=lon_max, lat_max=lat_max)
-
     segments = []
     for row in query_result:
         segments.append({
@@ -59,5 +58,4 @@ def get_geo():
             'osm_id': row[1],
             'coordinates': coordinates_from_linestring(row[2])
         })
-
     return jsonify({'segment_count': query_result.rowcount, 'segments': segments})
