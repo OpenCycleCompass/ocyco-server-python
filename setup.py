92be06506a76d7e193d17416caa03a24cd2a4002
Raphael Lehmann <raphael@rleh.de>
2016-12-18 17:47:06 +0100

    Update setup.py package list

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name='OCYCO-Server',
     version='0.0.2',
-    packages=['app', 'app.cost_dynamic', 'app.cost_dynamic_precalculated', 'app.cost_static', 'app.geo',
-              'app.profile_descriptions', 'app.profiles', 'app.track_points', 'app.tracks', 'app.users',
-              'app.way_type_descriptions', 'app.way_types'],
+    packages=['app', 'app.about', 'app.cost_dynamic', 'app.cost_dynamic_precalculated', 'app.cost_static', 'app.geo',
+              'app.geocoding', 'app.profile_descriptions', 'app.profiles', 'app.routing', 'app.track_points',
+              'app.tracks', 'app.users', 'app.way_type_descriptions', 'app.way_types'],
     url='https://github.com/OpenCycleCompass/ocyco-server-python',
     license='AGPLv3',
     author='Raphael Lehmann',
