aa3412164babaa7f1b0d5388b063b777a73592cc
Raphael Lehmann <raphael@rleh.de>
2016-12-18 17:47:59 +0100

    more unique name for testing database

--- config.travis.py
+++ config.travis.py
@@ -3,7 +3,7 @@ SQLALCHEMY_ECHO = False
 
 ADMINS = frozenset(['postmaster+travis-testing@open-cycle-compass.de'])
 
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost/ocyco'
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres@localhost/ocyco_pytest'
 DATABASE_CONNECT_OPTIONS = {}
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
