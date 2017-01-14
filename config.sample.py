0b3fab5b44dd47670a43593af01d5ba638c9a740
Raphael Lehmann <raphael@rleh.de>
2017-01-14 23:25:33 +0100

    restructure: create config module with config loader and default + testing configs

--- config.sample.py
+++
@@ -1,9 +0,0 @@
-DEBUG = True
-SQLALCHEMY_ECHO = True
-
-ADMINS = frozenset(['postmaster+pythonserver@open-cycle-compass.de'])
-
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:testing@localhost/ocyco'
-DATABASE_CONNECT_OPTIONS = {}
-
-SQLALCHEMY_TRACK_MODIFICATIONS = True
