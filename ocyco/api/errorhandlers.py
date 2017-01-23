f805258e59326a2e6f99817c81347d0b49c62e95
Alissa Wenzel <alissa.wenzel@rwth-aachen.de>
2017-01-23 19:00:02 +0100

    unified error management and representation

--- ocyco/api/errorhandlers.py
+++ ocyco/api/errorhandlers.py
@@ -9,50 +9,34 @@ def register(app):
     :type app: flask.Flask
     """
 
+    # errorhandlers for Flask Exceptions
+    @app.errorhandler(ParameterInvalidException)
+    @app.errorhandler(ParameterMissingException)
+    @app.errorhandler(NotFoundException)
+    @app.errorhandler(ConflictExistingObjectException)
+    @app.errorhandler(MultipleMatchesException)
+    @app.errorhandler(PhotonException)
+    def error_ocyco_exception(error):
+        return jsonify(error.to_dict()), error.status_code
+
+    @app.errorhandler(400)
+    @app.errorhandler(401)
+    @app.errorhandler(404)
+    @app.errorhandler(405)
+    @app.errorhandler(409)
+    @app.errorhandler(500)
+    def error_by_code(error):
+        return jsonify(error=error.description), error.code
+
     @app.errorhandler(TypeError)
     @app.errorhandler(ValueError)
-    def raise_bad_request(e):
-        return jsonify(message=e.message), 400
+    def error_bad_request(e):
+        return jsonify(error=e.message), 400
 
     @app.errorhandler(LookupError)
-    def raise_not_found(e):
-        return jsonify(message=e.message), 404
+    def error_not_found(e):
+        return jsonify(error=e.message), 404
 
-
-# errorhandlers for Flask Exceptions
-
-    @app.errorhandler(ParameterInvalidException)
-    def handle_parameter_invalid_exception(error):
-        response = jsonify(error.to_dict())
-        response.status_code = error.status_code
-        return response
-
-    @app.errorhandler(ParameterMissingException)
-    def handle_parameter_missing_exception(error):
-        response = jsonify(error.to_dict())
-        response.status_code = error.status_code
-        return response
-
-    @app.errorhandler(NotFoundException)
-    def handle_not_found_exception(error):
-        response = jsonify(error.to_dict())
-        response.status_code = error.status_code
-        return response
-
-    @app.errorhandler(ConflictExistingObjectException)
-    def handle_conflict_existing_object_exception(error):
-        response = jsonify(error.to_dict())
-        response.status_code = error.status_code
-        return response
-
-    @app.errorhandler(MultipleMatchesException)
-    def handle_multiple_matches_exception(error):
-        response = jsonify(error.to_dict())
-        response.status_code = error.status_code
-        return response
-
-    @app.errorhandler(PhotonException)
-    def handle_photon_exception(error):
-        response = jsonify(error.to_dict())
-        response.status_code = error.status_code
-        return response
+    @app.errorhandler(Exception)
+    def error_unknown(e):
+        return jsonify(error=e.message), 500
--- ocyco/api/exceptions.py
+++ ocyco/api/exceptions.py
@@ -7,7 +7,7 @@ class OcycoException(Exception):
 
     def to_dict(self):
         rv = dict(self.payload or())
-        rv['message'] = self.message
+        rv['error'] = self.message
         return rv
 
 
